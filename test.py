4d078f1e9885b33e13fe76c31cb742d5d63bb03a
Danila <d.kozlov@isimplelab.com>
2018-01-24 23:14:33 +0300

    Добавлены все остальные файлы с данными.

--- test.py
+++ test.py
@@ -1,14 +1,23 @@
 import pandas as pd
 
 # чтение из файла
-fixed_df = pd.read_csv('data/test.csv')
+# Все файлы, что нужно прочитать, дабы собрать с них данные
+holidays_events = pd.read_csv('data/holidays_events.csv')
+items = pd.read_csv('data/items.csv')
+oil = pd.read_csv('data/oil.csv')
+sample_submission = pd.read_csv('data/sample_submission.csv')
+stores = pd.read_csv('data/stores.csv')
+test = pd.read_csv('data/test.csv')
+train = pd.read_csv('data/train.csv')
+train2 = pd.read_csv('data/train2.csv')
+transactions = pd.read_csv('data/transactions.csv')
 
-# вывод колонки по имени
-# fixed_df['id']
+
+#  Теперь стоило бы понять, что с этим всем делать :D
 
 # так берёт каждый элемент по колонке
-for i in fixed_df['id']:
-    print(i)
+# for i in fixed_df['id']:
+#     print(i)
 
 # как выводить график пока не понятно
 # fixed_df.plot()
