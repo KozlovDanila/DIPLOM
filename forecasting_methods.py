9f76b6f00bf9a40fa6a73de1cc5eeb80479fb892
Danila <d.kozlov@isimplelab.com>
2018-04-19 23:54:10 +0300

    Добавлен метод Виллимейна.

--- forecasting_methods.py
+++ forecasting_methods.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 import numpy as np
 
@@ -15,8 +16,9 @@ def simple(data, is_day=False):
 # Метод Эфрона
 def efron(data, is_day=False, is_smooth=False):
     data = list(data)
-    if is_smooth:
-        data = smooth(data)
+    data = willemain(data)
+    # if is_smooth:
+    #     data = smooth(data)
     result = []
     for i in range(0, ITERATION_COUNT):
         var = sum(np.random.choice(data, size=DAYS_OF_WEEK))
@@ -34,7 +36,7 @@ def efron(data, is_day=False, is_smooth=False):
 # 4. Берём интервал с вероятность 1 - p и заполняем a+(b-a)*r
 
 def smooth(data):
-    copy_array = np.copy(list(data))
+    copy_array = np.copy(data)
     copy_array.sort()
     data_range = get_data_range(copy_array)
     probability = get_probability(copy_array, data_range)
@@ -69,3 +71,21 @@ def get_interval(data, value):
         if data[i] > value:
             return data[i - 1], data[i]
         i += 1
+
+
+# СГЛАЖИВАНИЕ методом Вилемайна
+# 1. Берём x из данных и заменяем на формулу x + N(0,1) * sqrt(x) + 1
+# если число < 0, оставляем как есть
+# N(0,1) = 1/sqrt(2*П) * e^-x^2/2
+
+def norm(x):
+    # return (1 / np.math.sqrt(2 * np.math.pi)) * (np.math.e ** -((x ** x) / 2))
+    return random.random()
+
+def willemain(data):
+    # Сколько чисел брать
+    for i in range(len(data) // 3):
+        index = random.randint(0, len(data)-1)
+        x = data[index]
+        data[index] = x + norm(x) * math.sqrt(x) + 1
+    return data
